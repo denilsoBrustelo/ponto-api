23502aaa0e13e04d8afe6f64d3778ec6af1f3b79
Alessandro Menezes <alessandromenezes22@gmail.com>
2017-10-21 00:07:18 -0300

    Uso do profile de test nos testes

--- src/test/java/br/com/greenmile/ponto_api/common/utils/BCryptUtilTests.java
+++ src/test/java/br/com/greenmile/ponto_api/common/utils/BCryptUtilTests.java
@@ -3,12 +3,14 @@ package br.com.greenmile.ponto_api.common.utils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class BCryptUtilTests {
 
     @Test
--- src/test/java/br/com/greenmile/ponto_api/common/utils/EntityUtilTests.java
+++ src/test/java/br/com/greenmile/ponto_api/common/utils/EntityUtilTests.java
@@ -4,12 +4,14 @@ import br.com.greenmile.ponto_api.domain.Usuario;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class EntityUtilTests {
 
     @Test
