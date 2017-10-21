53841906ab15bd9c8df42a5b5e1b1992f49630e9
Alessandro Menezes <alessandromenezes22@gmail.com>
2017-10-21 00:01:29 -0300

    Profile de teste

--- src/test/java/br/com/greenmile/ponto_api/PontoApiApplicationTests.java
+++ src/test/java/br/com/greenmile/ponto_api/PontoApiApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class PontoApiApplicationTests {
 
 	@Test
